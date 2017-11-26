7793996ba9e1a835d97dc6137b12ddba1a6ebd58
xiaolumei <liulu_xiaolu@126.com>
2017-11-26 17:45:04 +0800

    xinjianfa

--- src/com/atguigu/git/NewJianFa.java
+++ src/com/atguigu/git/NewJianFa.java
@@ -2,6 +2,6 @@ package com.atguigu.git;
 
 public class NewJianFa {
 public static void main(String[] args) {
-	System.out.println("hello new jianfa");
+	System.out.println("hello newjianfa");
 }
 }
