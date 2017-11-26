b9d3fdcd1e7ffe06c8e1092848b84be3a3853704
xiaolumei <liulu_xiaolu@126.com>
2017-11-26 17:19:32 +0800

    10jian

--- src/com/atguigu/git/Dugu9jian.java
+++ src/com/atguigu/git/Dugu9jian.java
@@ -2,6 +2,10 @@ package com.atguigu.git;
 
 public class Dugu9jian {
 	public static void main(String[] args) {
-		System.out.println("hello 9jian");
+		System.out.println("hello 10jian");
+		System.out.println("hello 10jian");
+		System.out.println("hello 10jian");
+		System.out.println("hello 10jian");
+
 	}
 }
